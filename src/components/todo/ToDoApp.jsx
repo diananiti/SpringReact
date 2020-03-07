65824778ae86101ada49472355a14924edac0b80
DianaNiti <diana.niti.14@gmail.com>
2020-03-07 15:56:46 +0100

    generalising handleChange

--- src/components/todo/ToDoApp.jsx
+++ src/components/todo/ToDoApp.jsx
@@ -21,32 +21,43 @@ this.state={
     username:'diana',
     password:''
 }
-this.handleUsernameChange=this.handleUsernameChange.bind(this);
-this.handlePasswordChange=this.handlePasswordChange.bind(this);
+//this.handleUsernameChange=this.handleUsernameChange.bind(this);
+//this.handlePasswordChange=this.handlePasswordChange.bind(this);
+this.handleChange=this.handleChange.bind(this);
 }
-handleUsernameChange(event){
-console.log(event.target.value);//gives the value which is being changed(int he console debug)
-this.setState(
-    {
-        username:event.target.value
-    }
-    //whenever username is changed , the state is updated 
-)
-}
-handlePasswordChange(event){
-    console.log(event.target.value);
+
+handleChange(event){
+    console.log(event.target.name);//gives the value which is being changed(int he console debug)
     this.setState(
         {
-           password:event.target.value 
+          [event.target.name]:event.target.value
         }
+        //whenever username is changed , the state is updated 
     )
-}
+    }
+// handleUsernameChange(event){
+// console.log(event.target.name);//gives the value which is being changed(int he console debug)
+// this.setState(
+//     {
+//       [event.target.name]:event.target.value
+//     }
+//     //whenever username is changed , the state is updated 
+// )
+// }
+// handlePasswordChange(event){
+//     console.log(event.target.value);
+//     this.setState(
+//         {
+//            password:event.target.value 
+//         }
+//     )
+// }
 
     render(){
         return(
             <div className="ToDoApp">
-           User Name: <input type="text" name ="username" value= {this.state.username} onChange={this.handleUsernameChange}/>
-           Password : <input type="password" name="password"value={this.state.password} onChange={this.handlePasswordChange}/>
+           User Name: <input type="text" name ="username" value= {this.state.username} onChange={this.handleChange}/>
+           Password : <input type="password" name="password"value={this.state.password} onChange={this.handleChange}/>
            <button>
                Login
            </button>
