d4bddf74bb50d71a0b8dcc676135977fbd2797c3
DianaNiti <diana.niti.14@gmail.com>
2020-03-07 15:42:33 +0100

    Adding controlled components -username and password

--- src/components/todo/ToDoApp.jsx
+++ src/components/todo/ToDoApp.jsx
@@ -12,28 +12,41 @@ class ToDoApp extends Component{
 
 
 class LoginComponent extends Component{
-constructor(props){//any properties are passed on through super class
+
+
+
+constructor(props){//any properties are passed on through super class constructor
 super(props);
 this.state={
     username:'diana',
     password:''
 }
 this.handleUsernameChange=this.handleUsernameChange.bind(this);
+this.handlePasswordChange=this.handlePasswordChange.bind(this);
 }
 handleUsernameChange(event){
-console.log(event.target.value);
+console.log(event.target.value);//gives the value which is being changed(int he console debug)
 this.setState(
     {
         username:event.target.value
     }
+    //whenever username is changed , the state is updated 
 )
+}
+handlePasswordChange(event){
+    console.log(event.target.value);
+    this.setState(
+        {
+           password:event.target.value 
+        }
+    )
 }
 
     render(){
         return(
-            <div>
-           User Name: <input type="text" namae ="username" value= {this.state.username} onChange={this.handleUsernameChange}/>
-           Password : <input type="text" name="password"value={this.state.password} onChange={this.handlePasswordChange}/>
+            <div className="ToDoApp">
+           User Name: <input type="text" name ="username" value= {this.state.username} onChange={this.handleUsernameChange}/>
+           Password : <input type="password" name="password"value={this.state.password} onChange={this.handlePasswordChange}/>
            <button>
                Login
            </button>
