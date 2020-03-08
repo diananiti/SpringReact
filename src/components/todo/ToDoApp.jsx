0a9a995900c8ebb8ee863c78f56bf4eaf84f6fcd
DianaNiti <diana.niti.14@gmail.com>
2020-03-08 12:52:18 +0100

    after bootstrapping

--- src/components/todo/ToDoApp.jsx
+++ src/components/todo/ToDoApp.jsx
@@ -63,9 +63,9 @@ class HeaderComponent extends Component {
 class FooterComponent extends Component {
     render() {
         return (
-           <footer className="footer">
-<spam className="text-muted">All Rights Reserved Diana </spam>
-           </footer>
+            <footer className="footer">
+                <span className="text-muted">All Rights Reserved Diana </span>
+            </footer>
         )
     }
 }
@@ -73,10 +73,10 @@ class LogoutComponent extends Component {
     render() {
         return (
             <div>
-              <h1>
-                  You are logged out
+                <h1>
+                    You are logged out
               </h1>
-              <div classname="container">Thank you </div>
+                <div classname="container">Thank you </div>
             </div>
         )
     }
@@ -102,47 +102,58 @@ class ListToDosComponent extends Component {
 
             <div>
                 <h1>List  Todos</h1>
-                <table>
-                    <thead>
-                        <tr>
+                <div className="container">
+                    <table className="table">
+                        <thead>
+                            <tr>
 
-                            <th>id</th>
-                            <th>description</th>
-                            <th>is comepleted</th>
-                            <th>targetDate</th>
+                                
+                                <th>description</th>
+                                <th>is comepleted</th>
+                                <th>targetDate</th>
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.todos.map(
-                            todos => <tr>
-                                <td>
-                                    {todos.id}
-                                </td>
-                                <td>
-                                    {todos.description}
-                                </td>
-                                <td>
-                                    {todos.done.toString()}
-                                </td>
-                                <td>
-                                    {todos.targetDate.toString()}
-                                </td>
                             </tr>
-                        )}
+                        </thead>
+                        <tbody>
+                            {this.state.todos.map(
+                                todos => <tr>
+                                  
+                                    <td>
+                                        {todos.description}
+                                    </td>
+                                    <td>
+                                        {todos.done.toString()}
+                                    </td>
+                                    <td>
+                                        {todos.targetDate.toString()}
+                                    </td>
+                                </tr>
+                            )}
 
-                    </tbody>
-                </table>
-
+                        </tbody>
+                    </table>
+                </div>
             </div>)
     }
 }
 class WelcomeComponent extends Component {
     render() {
-        return <div>
-            Welcome
+        return (
+            <>
+                <h1>
+                    Welcome
+           </h1>
+                <div className="container">
+                    Welcome
               {this.props.match.params.name}. You can manage your todos <Link to="/todos">here</Link>
-        </div>
+                </div>
+            </>
+        )
+
+
+
+
+
     }
 }
 
@@ -214,17 +225,21 @@ class LoginComponent extends Component {
 
     render() {
         return (
-            <div className="ToDoApp">
-                {/* <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}></ShowInvalidCredentials> */}
-                {this.state.hasLoginFailed && <div>Invalid Credentials</div>}
-                {/* <showSuccessMessage showSuccessMessage={this.state.showSuccessMessage}></showSuccessMessage> */}
-                {this.state.showSuccessMessage && <div> Succesful Login</div>}
+            <div >
+                <h1>Login</h1>
+                <div className="container">
+                    {/* <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}></ShowInvalidCredentials> */}
+                    {this.state.hasLoginFailed && <div className="alert-warning">Invalid Credentials</div>}
+                    {/* <showSuccessMessage showSuccessMessage={this.state.showSuccessMessage}></showSuccessMessage> */}
+                    {this.state.showSuccessMessage && <div > Succesful Login</div>}
 
-                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
-                Password : <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
-                <button onClick={this.loginClicked}>
-                    Login
+                    User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
+                    Password : <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+                    <button className="btn btn-success" onClick={this.loginClicked}>
+                        Login
            </button>
+                </div>
+
             </div>
         );
     }
